791d3bd4fe80e3b3d8dd01c83888c3f8ec0d9d4c
Marion DEBELLEY <mdebelle@e3r5p14.42.fr>
2015-03-24 16:02:32 +0100

    My version of opengl

--- lib/opengl/GlDisplay.cpp
+++ lib/opengl/GlDisplay.cpp
@@ -1,4 +1,9 @@
 #include "GlDisplay.h"
+#include <openGL/gl.h>
+#include <openGL/glu.h>	
+#include <GLUT/glut.h>
+
+
 
 GlDisplay::GlDisplay()
 {
@@ -12,8 +17,19 @@ GlDisplay::~GlDisplay()
 
 void	GlDisplay::init(int width, int height)
 {
-(void)width;
-(void)height;
+//	char	argv[1][10] = { "./Nibbler" };
+//	int		argc = 1;
+
+
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowSize(width, height); 
+	glutInitWindowPosition(100, 100); 
+	glutCreateWindow("Tutorial 01");
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glutMainLoop();
+	glClear(GL_COLOR_BUFFER_BIT); 
+	glutSwapBuffers();
+
 }
 
 void	GlDisplay::drawPattern(int posX, int posY, int sizeX, int sizeY, Pattern::Type type)
